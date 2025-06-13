e0546974c2b3517057bbaa5dffe620c4101b897c
Lovemore Jokonya <lovemorejokonya@gmail.com>
2025-06-13 17:07:53 -0400

    fixed port config for deployment

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const io = new Server(server, {
     }
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/api/health', (req, res) => {
     res.json({ message: "running" });
